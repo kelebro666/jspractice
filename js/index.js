3ec8002817ef5b158c9c6b3c3c363dc3b10ea820
kelebro666 <kelebro666@gmail.com>
2020-02-11 21:49:23 +0000

    Comment

--- js/index.js
+++ js/index.js
@@ -1,24 +1,25 @@
 const link = document.querySelector('a');
 
-link.textContent = 'my page facebook';
-link.href = 'https://www.facebook.com/ihor.muliar.13';
+link.textContent = 'Become true JS Jedi!';
+link.href = 'https://sj42.programmingmentor.com';
 
 const sect = document.querySelector('section');
-const para = document.createElement ('p');
-sect.appendChild(para)
+const para = document.createElement('p');
+sect.appendChild(para);
 para.textContent = 'We hope you enjoyed the ride.';
+
+// Task - make 1000 paragraphs
+
+// [...Array(1000)].forEach( (_, i) => {
+//     const p = document.createElement('p');
+//     p.textContent = i + 1 + ' lorem ipsum...';
+//     sect.appendChild(p);
+// });
 
 const linkPara = document.querySelector('p');
 sect.appendChild(linkPara.cloneNode(true));
 
-
-
-
-
-const clock = document.querySelector('.clock');
-const showClock = () => clock.innerText = (new Date()).toLocaleTimeString();
-setInterval(showClock, 1000);
-
+// linkPara.parentNode.removeChild(linkPara);
 linkPara.remove();
 
 para.style.color = 'white';
@@ -27,7 +28,16 @@ para.style.padding = '10px';
 para.style.width = '250px';
 para.style.textAlign = 'center';
 
-$('.click-me').click( () => $('img').toggle(3000) )
+para.classList.add('formatted');
+
+const clock = document.querySelector('.clock');
+const options = {  
+    hour: "2-digit", minute: "2-digit", second: "2-digit"
+};  
+const showClock = () => clock.innerText = (new Date()).toLocaleTimeString("uk", options);
+setInterval(showClock, 1000);
+
+$('.click-me').click( () => $('img').toggle(3000) );
 
 const display = document.querySelector('.display');
 
@@ -46,7 +56,6 @@ function operPressed(ev) {
 }
 
 document.querySelector('.eq').addEventListener('click', calculate);
-
 function calculate() {
     display.val = eval(display.val);
 }
